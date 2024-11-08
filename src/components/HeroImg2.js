1316a81fb18bf30aad4d1b7e52487aa8e5c28b6b
Meiddiel <tokymickaelrivo@gmail.com>
2024-11-08 22:34:47 +0300

    create another heroImg
    it's to differenciat home between other route

---
+++ src/components/HeroImg2.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HeroimgStyles2.css";
+
+const HeroImg2 = () => {
+  return <div className="hero-img">
+    <div className="heading">
+      <h1>This is a Title</h1>
+      <p>this is some text</p>
+    </div>
+  </div>
+};
+
+export default HeroImg2;
