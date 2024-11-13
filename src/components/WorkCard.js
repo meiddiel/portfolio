fc148aacf75ba0b73c0f091485760f1ba7658cad
Meiddiel <tokymickaelrivo@gmail.com>
2024-11-13 22:20:51 +0300

    creat work card

---
+++ src/components/WorkCard.js
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import "./WorkCardStyles.css";
+import pro1 from "../assets/BG.jpg";
+
+import React from "react";
+
+const WorkCard = () => {
+  return (
+    <div className="work-container">
+      <h1 className="project-heading">Projects</h1>
+      <div className="project-container">
+        <div className="project-card">
+          <img src={pro1} alt="imageBg" />
+          <h2 className="project-title">Project Title</h2>
+          <div className="pro-details">
+            <p>This is text</p>
+            <div className="pro-btns">
+              <NavLink to="url.com" className="btn">View</NavLink>
+              <NavLink to="url.com" className="btn">source</NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WorkCard;
