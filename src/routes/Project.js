949f6ede94de2beb6036b07add8b5f5982ba3085
Meiddiel <tokymickaelrivo@gmail.com>
2024-11-13 21:16:00 +0300

    adding pricing card to our project

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "../components/Footer";
 import HeroImg2 from "../components/HeroImg2";
 import Navbar from "../components/Navbar";
+import PricingCard from "../components/PricingCard";
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="PROJECTS." text="Some of my most recent works" />
+      <PricingCard />
       <Footer />
     </div>
   );
